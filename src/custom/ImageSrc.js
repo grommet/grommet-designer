a534e374002602457919d758ef968ebbc0dffeb0
Eric Soderberg <eric.soderberg@hpe.com>
2019-06-11 21:45:20 -0700

    variable rename

--- src/custom/ImageSrc.js
+++ src/custom/ImageSrc.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Box, Paragraph, TextArea } from 'grommet';
 
-const prefix = "data:image/svg+xml;utf8,";
+const svgPrefix = "data:image/svg+xml;utf8,";
 
 const stripPrefix = value => {
-  if (value && value.slice(0, prefix.length) === prefix) {
-    return value.slice(prefix.length);
+  if (value && value.slice(0, svgPrefix.length) === svgPrefix) {
+    return value.slice(svgPrefix.length);
   }
   return value || '';
 }
@@ -23,7 +23,7 @@ export default ({ value, onChange }) => {
         onChange={(event) => {
           const nextValue = event.target.value;
           if (nextValue[0] === '<') {
-            onChange(`${prefix}${nextValue}`);
+            onChange(`${svgPrefix}${nextValue}`);
           } else {
             onChange(nextValue);
           }
