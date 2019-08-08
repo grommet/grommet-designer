0f9f7915f7943b07a74be895d1732ba8e9ae2937
Eric Soderberg <eric.soderberg@hpe.com>
2019-08-08 16:32:09 -0700

    Added standard designs

--- src/Tree/Designs.js
+++ src/Tree/Designs.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Box, Button, Grid, Stack, Text, grommet } from 'grommet';
+import { Box, Button, Grid, Heading, Stack, Text, grommet } from 'grommet';
 import { Trash } from 'grommet-icons';
-import { bare, getInitialSelected, resetState, upgradeDesign } from '../design';
+import {
+  apiUrl, bare, getInitialSelected, resetState, upgradeDesign,
+} from '../design';
 import Action from '../components/Action';
 import ActionButton from '../components/ActionButton';
+
+const standardDesigns = [
+  {
+    name: 'Navigation Patterns',
+    id: 'Navigation-Patterns-eric-soderberg-hpe-com',
+  },
+  {
+    name: 'Card',
+    id: 'Card-eric-soderberg-hpe-com',
+  }
+];
 
 const nameToBackground = (name) => {
   let num = 0;
@@ -13,10 +26,29 @@ const nameToBackground = (name) => {
   return `accent-${(num % 4) + 1}`;
 };
 
+const Design = ({ name, loading, onClick }) => (
+  <Box fill round="medium" overflow="hidden" animation={loading ? 'pulse' : undefined}>
+    <Button fill plain onClick={onClick}>
+      {({ hover }) => (
+        <Box
+          fill
+          pad="medium"
+          background={hover ? 'light-1' : nameToBackground(name)}
+          align="center"
+          justify="center"
+        >
+          <Text textAlign="center" weight="bold">{name}</Text>
+        </Box>
+      )}
+    </Button>
+  </Box>
+);
+
 const Designs = ({ design, onClose, onChange }) => {
   const [designs, setDesigns] = React.useState([]);
   const [error, setError] = React.useState();
   const [confirmDelete, setConfirmDelete] = React.useState();
+  const [loading, setLoading] = React.useState();
 
   React.useEffect(() => {
     let item = localStorage.getItem('designs'); // array of names
@@ -58,29 +90,31 @@ const Designs = ({ design, onClose, onChange }) => {
     }
   }
 
+  const onLoad = (id) => {
+    setLoading(id);
+    fetch(`${apiUrl}/${id}`)
+      .then(response => response.json())
+      .then((nextDesign) => {
+        upgradeDesign(nextDesign);
+        onChange({
+          design: nextDesign,
+          selected: getInitialSelected(nextDesign),
+        });
+        onClose();
+      })
+      .catch(() => setLoading(undefined));
+  }
+
   return (
     <Action label="designs" onClose={onClose} full="horizontal">
-      <Grid fill="horizontal" columns="small" rows="small" gap="large">
+      <Heading level={2}>My Designs</Heading>
+      <Grid fill="horizontal" columns="small" rows="xsmall" gap="large">
         <Box fill round="medium" >
           <Button fill label="New" onClick={onReset} />
         </Box>
         {designs.map(name => (
           <Stack key={name} fill anchor="bottom-right">
-            <Box fill round="medium" overflow="hidden">
-              <Button fill plain onClick={() => onSelect(name)}>
-                {({ hover }) => (
-                  <Box
-                    fill
-                    pad="medium"
-                    background={hover ? 'light-1' : nameToBackground(name)}
-                    align="center"
-                    justify="center"
-                  >
-                    <Text textAlign="center" weight="bold">{name}</Text>
-                  </Box>
-                )}
-              </Button>
-            </Box>
+            <Design name={name} onClick={() => onSelect(name)} />
             <Box direction="row" gap="small">
               {confirmDelete === name && (
                 <ActionButton
@@ -94,7 +128,8 @@ const Designs = ({ design, onClose, onChange }) => {
                 title="delete design"
                 icon={<Trash color="dark-3" />}
                 hoverIndicator
-                onClick={() => setConfirmDelete(name)}
+                onClick={() =>
+                  setConfirmDelete(confirmDelete === name ? undefined : name)}
               />
             </Box>
           </Stack>
@@ -126,21 +161,34 @@ const Designs = ({ design, onClose, onChange }) => {
               }}
             />
             <Box
-                fill
-                background="dark-1"
-                align="center"
-                justify="center"
-              >
-                <Text>Import</Text>
-                {error && (
-                  <Box background="status-critical" pad="medium">
-                    <Text>{error}</Text>
-                  </Box>
-                )}
-              </Box>
+              fill
+              background="dark-1"
+              align="center"
+              justify="center"
+            >
+              <Text>Import</Text>
+              {error && (
+                <Box background="status-critical" pad="medium">
+                  <Text>{error}</Text>
+                </Box>
+              )}
+            </Box>
           </Stack>
         </Box>
       </Grid>
+      <Box alignSelf="stretch" border="top" margin={{ top: 'large' }}>
+        <Heading level={2}>Starter Designs</Heading>
+        <Grid fill="horizontal" columns="small" rows="xsmall" gap="large">
+          {standardDesigns.map(({ name, id }) => (
+            <Design
+              key={id}
+              name={name}
+              loading={loading === id}
+              onClick={() => onLoad(id)}
+            />
+          ))}
+        </Grid>
+      </Box>
     </Action>
   );
 };
