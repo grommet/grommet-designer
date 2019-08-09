66472484b5c292ada13c377dc277ffd1d67a927d
Eric Soderberg <eric.soderberg@hpe.com>
2019-08-08 18:22:59 -0700

    blank page for bare

--- src/design/bare.js
+++ src/design/bare.js
@@ -3,7 +3,8 @@ export const bare = {
   screens: { 1: { id: 1, root: 2 } },
   screenOrder: [1],
   components: {
-    2: { id: 2, type: 'Grommet', props: { style: { height: '100vh' } } },
+    2: { id: 2, type: 'Grommet', props: { style: { height: '100vh' } }, children: [ 3 ] },
+    3: { id: 3, type: 'Heading', props: { textAlign: 'center' }, text: 'blank page' },
   },
   theme: 'grommet',
 };
