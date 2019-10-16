8089e195922239bc52525ff4ef5606d53439694b
Eric Soderberg <eric.soderberg@hpe.com>
2019-10-16 10:35:19 -0700

    working on Router

--- src/Router.js
+++ src/Router.js
@@ -15,18 +15,22 @@ export const Router = ({ children }) => {
       const { location } = document;
       setPath(location.pathname);
       setSearch(location.search);
-    }
+    };
 
     window.addEventListener('popstate', onPopState);
-    return (() => window.removeEventListener('popstate', onPopState));
-  }, [])
+    return () => window.removeEventListener('popstate', onPopState);
+  }, []);
 
   const onPush = nextPath => {
     if (nextPath !== path) {
       if (nextPath.startsWith('http')) {
         window.location = nextPath;
       } else {
-        window.history.pushState(undefined, undefined, `${nextPath}${search || ''}`);
+        window.history.pushState(
+          undefined,
+          undefined,
+          `${nextPath}${search || ''}`,
+        );
         setPath(nextPath);
         window.scrollTo(0, 0);
       }
@@ -38,68 +42,62 @@ export const Router = ({ children }) => {
       {children}
     </RouterContext.Provider>
   );
-}
+};
 
 Router.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export const Routes = ({ children, notFoundRedirect }) => (
-  <RouterContext.Consumer>
-    {({ path: currentPath }) => {
-      const preHash = currentPath && currentPath.split('#')[0];
-      let found;
-      Children.forEach(children, child => {
-        if (found || !child) return;
-        const { path, exact } = child.props;
-        const prefix = path ? path.split(':')[0] : '';
-        if (
-          !found &&
-          currentPath &&
-          ((exact && preHash === path) || (!exact && preHash.startsWith(prefix)))
-        ) {
-          found = child;
-        }
-      });
-      if (currentPath && !found) {
-        window.history.replace(notFoundRedirect);
-      }
-      return found;
-    }}
-  </RouterContext.Consumer>
-);
+export const Routes = ({ children, notFoundRedirect }) => {
+  const { path: currentPath } = React.useContext(RouterContext);
+  const preHash = currentPath && currentPath.split('#')[0];
+  let found;
+  Children.forEach(children, child => {
+    if (found || !child) return;
+    const { path, exact } = child.props;
+    const prefix = path ? path.split(':')[0] : '';
+    if (
+      !found &&
+      currentPath &&
+      ((exact && preHash === path) || (!exact && preHash.startsWith(prefix)))
+    ) {
+      found = child;
+    }
+  });
+  if (currentPath && !found) {
+    window.history.replace(notFoundRedirect);
+  }
+  return found;
+};
 
 Routes.propTypes = {
   children: PropTypes.node.isRequired,
   notFoundRedirect: PropTypes.string.isRequired,
 };
 
-export const Route = ({ component: Comp, exact, path, redirect }) => (
-  <RouterContext.Consumer>
-    {({ path: currentPath }) => {
-      const preHash = currentPath.split('#')[0];
-      const prefix = !exact && path.split(':')[0];
-      if (currentPath && (
-        (exact && preHash === path)
-        || (!exact && preHash.startsWith(prefix))
-      )) {
-        if (redirect) {
-          window.history.replace(redirect);
-        } else if (Comp) {
-          const props = {};
-          if (!exact) {
-            const propName = path.split(':')[1];
-            props[propName] = preHash.slice(prefix.length);
-          }
-          return <Comp {...props} />;
-        } else {
-          console.error('Route missing component or redirect');
-        }
+export const Route = ({ component: Comp, exact, path, redirect }) => {
+  const { path: currentPath } = React.useContext(RouterContext);
+  const preHash = currentPath.split('#')[0];
+  const prefix = !exact && path.split(':')[0];
+  if (
+    currentPath &&
+    ((exact && preHash === path) || (!exact && preHash.startsWith(prefix)))
+  ) {
+    if (redirect) {
+      window.history.replace(redirect);
+    } else if (Comp) {
+      const props = {};
+      if (!exact) {
+        const propName = path.split(':')[1];
+        props[propName] = preHash.slice(prefix.length);
       }
-      return null;
-    }}
-  </RouterContext.Consumer>
-);
+      return <Comp {...props} />;
+    } else {
+      console.error('Route missing component or redirect');
+    }
+  }
+  return null;
+};
 
 Route.propTypes = {
   component: PropTypes.func,
