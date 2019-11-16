3116db3341c74a7d544b5f31f019bd35a88e89c2
Eric Soderberg <eric.soderberg@hpe.com>
2019-11-15 17:30:00 -0800

    Better tooltips for action buttons

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Box, Button } from 'grommet';
+import { Box, Button, Drop, Text } from 'grommet';
 
-export default (props) => (
-  <Box flex={false}>
-    <Button hoverIndicator {...props} />
-  </Box>
-);
+export default ({ title, ...rest }) => {
+  const ref = React.useRef();
+  const [hover, setHover] = React.useState();
+
+  React.useEffect(() => {
+    if (!hover) return;
+    const timer = setTimeout(() => setHover(false), 3000);
+    return () => clearTimeout(timer);
+  }, [hover]);
+
+  return (
+    <Box flex={false}>
+      <Button
+        ref={ref}
+        hoverIndicator
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        {...rest}
+      />
+      {hover && (
+        <Drop target={ref.current} align={{ top: 'bottom' }} plain>
+          <Box
+            margin="xsmall"
+            animation="fadeIn"
+            background={{ color: 'light-1', opacity: 'strong' }}
+            pad="xsmall"
+          >
+            <Text>{title}</Text>
+          </Box>
+        </Drop>
+      )}
+    </Box>
+  );
+};
