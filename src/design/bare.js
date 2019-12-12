ef4fb1dfa5fea48e2f4cc7301d4093c7875845c3
Eric Soderberg <eric.soderberg@hpe.com>
2019-12-12 06:44:43 -0800

    Added flex='grow' to initial Screen Box

--- src/design/bare.js
+++ src/design/bare.js
@@ -9,8 +9,52 @@ export const bare = {
         fill: 'vertical',
         overflow: 'auto',
         align: 'center',
+        flex: 'grow',
       },
     },
   },
+  // components: {
+  //   2: {
+  //     id: 2,
+  //     type: 'grommet.Main',
+  //     props: {
+  //       fill: 'vertical',
+  //       flex: 'grow',
+  //       overflow: 'auto'
+  //     },
+  //     children: [3, 4, 5],
+  //   },
+  //   3: {
+  //     id: 3,
+  //     type: 'grommet.Header',
+  //     props: {
+  //       align: 'center',
+  //       direction: 'row',
+  //       flex: false,
+  //       justify: 'between',
+  //       gap: 'medium'
+  //     },
+  //   },
+  //   4: {
+  //     id: 4,
+  //     type: 'grommet.Box',
+  //     props: {
+  //       align: 'center',
+  //       justify: 'center',
+  //       flex: 'grow',
+  //     },
+  //   },
+  //   5: {
+  //     id: 5,
+  //     type: 'grommet.Footer',
+  //     props: {
+  //       align: 'center',
+  //       direction: 'row',
+  //       flex: false,
+  //       justify: 'between',
+  //       gap: 'medium'
+  //     },
+  //   },
+  // },
   theme: 'grommet',
 };
