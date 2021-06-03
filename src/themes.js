e61ce87bf75b09ea72c383f0ed5cab9ba58d6950
Eric Soderberg <eric.soderberg@hpe.com>
2021-06-02 19:42:38 -0700

    Switch hpe-next theme to stable branch

--- src/themes.js
+++ src/themes.js
@@ -47,22 +47,21 @@ const themes = [
     label: 'hpe-next',
     name: 'hpe',
     packageName: 'grommet-theme-hpe',
-    packageUrl:
-      'https://github.com/grommet/grommet-theme-hpe/tarball/NEXT-stable',
+    packageUrl: 'https://github.com/grommet/grommet-theme-hpe/tarball/stable',
     jsUrl:
-      'https://grommet.github.io/grommet-theme-hpe/grommet-theme-hpe-next.min.js',
+      'https://grommet.github.io/grommet-theme-hpe/grommet-theme-hpe-2.min.js',
   },
 ];
 
 export default themes;
 
-export const themeForValue = value =>
+export const themeForValue = (value) =>
   themes.find(
-    theme =>
+    (theme) =>
       theme.packageName === value ||
       theme.jsUrl === value ||
       theme.designerUrl === value,
   );
 
-export const themeForUrl = url =>
-  themes.find(theme => url.search(`/${theme.packageName}/`) !== -1);
+export const themeForUrl = (url) =>
+  themes.find((theme) => url.search(`/${theme.packageName}/`) !== -1);
