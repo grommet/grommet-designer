a9a55c923375390a568841c92f3dc98c6d6171e7
Eric Soderberg <eric.soderberg@hpe.com>
2022-03-06 18:12:30 -0800

    Remove workaround for grommet y bounds

--- src/libraries/grommet/DataChartBounds.js
+++ src/libraries/grommet/DataChartBounds.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { Box, FormField, RadioButtonGroup, TextInput } from 'grommet';
 
 const DataChartBounds = ({ value, onChange }) => {
-  // temporary, until grommet fix
-  const normalizeValue =
-    (value && value.y && value.y[0] > value.y[1] && { y: value.y.reverse() }) ||
-    value;
   return (
     <Box>
       <RadioButtonGroup
@@ -25,9 +21,9 @@ const DataChartBounds = ({ value, onChange }) => {
         <>
           <FormField label="max">
             <TextInput
-              value={normalizeValue.y[1]}
+              value={value.y[1]}
               onChange={(event) => {
-                const nextValue = JSON.parse(JSON.stringify(normalizeValue));
+                const nextValue = JSON.parse(JSON.stringify(value));
                 nextValue.y[1] = parseInt(event.target.value, 10);
                 onChange(nextValue);
               }}
@@ -35,9 +31,9 @@ const DataChartBounds = ({ value, onChange }) => {
           </FormField>
           <FormField label="min">
             <TextInput
-              value={normalizeValue.y[0]}
+              value={value.y[0]}
               onChange={(event) => {
-                const nextValue = JSON.parse(JSON.stringify(normalizeValue));
+                const nextValue = JSON.parse(JSON.stringify(value));
                 nextValue.y[0] = parseInt(event.target.value, 10);
                 onChange(nextValue);
               }}
