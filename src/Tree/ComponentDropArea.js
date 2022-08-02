c2c0bb72f3935dba2d6d3ad49185a8ec55a4c231
Eric Soderberg <eric.soderberg@hpe.com>
2022-08-01 19:00:58 -0700

    Align component drop area with component background indicator

--- src/Tree/ComponentDropArea.js
+++ src/Tree/ComponentDropArea.js
@@ -8,26 +8,29 @@ const ComponentDropArea = ({ id, where }) => {
   const [dragOver, setDragOver] = useState();
 
   return (
-    <Box
-      pad="xxsmall"
-      background={(dragOver && 'focus') || undefined}
-      onDragEnter={(event) => {
-        if (dragging && dragging !== id && getComponent(dragging)) {
-          event.preventDefault();
-          setDragOver(true);
-        }
-      }}
-      onDragOver={(event) => {
-        if (dragOver) event.preventDefault();
-      }}
-      onDragLeave={() => setDragOver(false)}
-      onDrop={() => {
-        if (dragOver) {
-          moveComponent(dragging, { [where]: id });
-          setDragOver(false);
-        }
-      }}
-    />
+    <Box direction="row" pad={{ left: 'xsmall' }} margin={{ left: 'medium' }}>
+      <Box
+        flex
+        pad="xxsmall"
+        background={(dragOver && 'focus') || undefined}
+        onDragEnter={(event) => {
+          if (dragging && dragging !== id && getComponent(dragging)) {
+            event.preventDefault();
+            setDragOver(true);
+          }
+        }}
+        onDragOver={(event) => {
+          if (dragOver) event.preventDefault();
+        }}
+        onDragLeave={() => setDragOver(false)}
+        onDrop={() => {
+          if (dragOver) {
+            moveComponent(dragging, { [where]: id });
+            setDragOver(false);
+          }
+        }}
+      />
+    </Box>
   );
 };
 
