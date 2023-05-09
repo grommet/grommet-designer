ae780da8328c49dd1d18ece69e43ce6a75d2c77f
Eric Soderberg <eric.soderberg@hpe.com>
2023-05-09 13:19:05 -0700

    Improve messaging in ConfirmDelete

--- src/Tree/ConfirmDelete.js
+++ src/Tree/ConfirmDelete.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Box, Button, Footer, Layer, Paragraph, Text } from 'grommet';
+import { Anchor, Box, Button, Footer, Layer, Paragraph, Text } from 'grommet';
 import { getDesign, removeDesign } from '../design2';
 
 const ConfirmDelete = ({ onClose, onDone }) => {
-  const name = getDesign().name;
+  const { date, name, publishedDate, publishedUrl } = getDesign();
+
+  let publishedState;
+  if (!publishedUrl) publishedState = 'This design has never been published.';
+  else if (date !== publishedDate)
+    publishedState = 'The local design has unpublished changes.';
+  else
+    publishedState = (
+      <>
+        The <Anchor href={publishedUrl}>published copy</Anchor> of this design
+        will still be available.
+      </>
+    );
 
   return (
     <Layer
@@ -13,13 +25,14 @@ const ConfirmDelete = ({ onClose, onDone }) => {
       onEsc={onClose}
       onClickOutside={onClose}
     >
-      <Box flex elevation="medium" pad="large" gap="medium">
-        <Paragraph>
+      <Box flex elevation="medium" pad="large" gap="small">
+        <Paragraph margin="none">
           Just checking, are you sure you want to delete
           <br />
           <Text weight="bold"> {name}</Text>?
         </Paragraph>
-        <Footer justify="start">
+        <Paragraph margin="none">{publishedState}</Paragraph>
+        <Footer justify="start" margin={{ top: 'medium' }}>
           <Button
             label="Yes, delete"
             primary
